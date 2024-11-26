87ac3448f5d9d1775f56a89943ddb30286652028
Feblcsack <alifianabdiel@gmail.com>
2024-11-26 15:02:59 +0700

    last

--- src/pages/Slider.tsx
+++ src/pages/Slider.tsx
@@ -1,38 +1,37 @@
 import { InfiniteSlider } from '../components/ui/infinite-slider';
+import { useState } from 'react';
+
+function Placeholder() {
+  return <div className='w-[120px] h-[120px] rounded-[4px] bg-gray-300 animate-pulse'></div>;
+}
+
+function ImageWithLoader({ src, alt }: { src: string; alt: string }) {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  return (
+    <div className='w-[120px] h-[120px] relative'>
+      {!isLoaded && <Placeholder />}
+      <img
+        src={src}
+        alt={alt}
+        className={`w-full h-full rounded-[4px] object-cover transition-opacity duration-300 ${
+          isLoaded ? 'opacity-100' : 'opacity-0'
+        }`}
+        onLoad={() => setIsLoaded(true)}
+      />
+    </div>
+  );
+}
 
 export function Slider() {
   return (
     <InfiniteSlider durationOnHover={75} gap={24}>
-      <img
-        src='/berita/1.jpeg'
-        alt='Dean blunt - Black Metal 2'
-        className='aspect-square w-[120px] rounded-[4px]'
-      />
-      <img
-        src='/slider/1.webp'
-        alt='Jungle Jack - JUNGLE DES ILLUSIONS VOL 2'
-        className='aspect-square w-[120px] rounded-[4px]'
-      />
-      <img
-        src='https://i.scdn.co/image/ab67616d00001e02ecdb8f824367a53468100faf'
-        alt='Yung Lean - Stardust'
-        className='aspect-square w-[120px] rounded-[4px]'
-      />
-      <img
-        src='https://i.scdn.co/image/ab67616d00001e021624590458126fc8b8c64c2f'
-        alt='Lana Del Rey - Ultraviolence'
-        className='aspect-square w-[120px] rounded-[4px]'
-      />
-      <img
-        src='https://i.scdn.co/image/ab67616d00001e020dcf0f3680cff56fe5ff2288'
-        alt='A$AP Rocky - Tailor Swif'
-        className='aspect-square w-[120px] rounded-[4px]'
-      />
-      <img
-        src='https://i.scdn.co/image/ab67616d00001e02bc1028b7e9cd2b17c770a520'
-        alt='Midnight Miami (feat Konvy) - Nino Paid, Konvy'
-        className='aspect-square w-[120px] rounded-[4px]'
-      />
+      <ImageWithLoader src='/berita/1.jpeg' alt='Dean blunt - Black Metal 2' />
+      <ImageWithLoader src='/slider/1.webp' alt='Jungle Jack - JUNGLE DES ILLUSIONS VOL 2' />
+      <ImageWithLoader src='/slider/ojek.jpg' alt='Yung Lean - Stardust' />
+      <ImageWithLoader src='/slider/3.jpeg' alt='Lana Del Rey - Ultraviolence' />
+      <ImageWithLoader src='/slider/4.webp' alt='A$AP Rocky - Tailor Swift' />
+      <ImageWithLoader src='/slider/5.webp' alt='Midnight Miami (feat Konvy) - Nino Paid, Konvy' />
     </InfiniteSlider>
   );
 }
